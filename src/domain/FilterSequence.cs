9ba231ee4e899c6b7bd8dddb80f4019070bd5d77
clodomir vianna da silva neto <clovianna_begins@hotmail.com>
2025-08-06 11:29:27 -0300

    Fixed issue with parsing filter element containing dash character.

--- src/domain/FilterSequence.cs
+++ src/domain/FilterSequence.cs
@@ -65,7 +65,6 @@ public class FilterSequence
                     return result;
                 }
 
-                return new List<string> { $"{startStr}-{endStr}" };
             }
         }
 
