d86abc36226e3cebd53f5f75560548c7e431f915
clodomir vianna da silva neto <clovianna_begins@hotmail.com>
2025-08-26 16:37:11 -0300

    Fixing configurations for single-file builds.

--- _commands/build/BuildProcess.ps1
+++ _commands/build/BuildProcess.ps1
@@ -12,26 +12,39 @@ function ExecuteBuildProcess {
     $allOutputs = @()
 
     foreach ($RuntimeId in $RuntimeIds) {
-
         $commonArgs = @(
-            $projectPath
-            "-c", $Configuration
-            "-p:Version=$Version"
-            "-p:InformationalVersion=$Version+build.$BuildId"
-            "-p:RuntimeIdentifier=$RuntimeId"
+            $projectPath,
+            "-c", $Configuration,
+            "-p:Version=$Version",
+            "-p:InformationalVersion=$Version+build.$BuildId",
+            "-p:RuntimeIdentifier=$RuntimeId",
             "-v:q"
         )
-
         if ($Configuration -ieq "Release") {
-            # Use dotnet publish for Release builds
+            # Normalize RIDs
+            if ($RuntimeId -eq "win10-x64") { $RuntimeId = "win-x64" }
+
+            # Specify target framework (hardcode or read from csproj)
+            $TargetFramework = "net8.0"
+
+            # Compose output path including RID and TFM
+            $OutputPath = Join-Path $PSScriptRoot "..\..\builds\last\Release\single-file\$TargetFramework\$RuntimeId"
+
             $publishArgs = $commonArgs + @(
                 "--self-contained", "true",
-                "--output", (Join-Path $PSScriptRoot "..\..\builds\last\bin\Release\$RuntimeId")
+                "-p:PublishSingleFile=true",
+                "-p:PublishTrimmed=false",
+                "-p:PublishReadyToRun=true",
+                "-p:IncludeAllContentForSelfExtract=true",
+                "-p:IncludeNativeLibrariesForSelfExtract=true",
+                "-p:DebugType=None",
+                "-p:DebugSymbols=false",
+                "--output", $OutputPath
             )
+
             $output = dotnet publish @publishArgs 2>&1
         }
         else {
-            # Use dotnet build for Debug builds
             $output = dotnet build @commonArgs 2>&1
         }
 
